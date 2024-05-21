e4f9ba6ced6d840964f2fdc73483e5a423aa660d
ServiceCommit <delboy@secure.homelab.net>
2024-05-21 16:16:39 +0100

    V1.1

--- static/script.js
+++
@@ -1,9 +0,0 @@
-/* This shows or hides the tutorial div on the cipher page when a button is pressed*/
-function showhidetutorial(){
-    var x = document.getElementById("tutorial");
-    if (x.style.display == "none"){
-        x.style.display = "block"
-    } else {
-        x.style.display = "none"
-    }
-}
